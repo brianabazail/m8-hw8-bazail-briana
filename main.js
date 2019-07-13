fd3de6dac504d8d80c9949d1a72147e9a13ef74c
Briana Bazail <brianabazail@ufl.edu>
2019-07-13 18:22:24 -0400

    Add js functions

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 // Global Variables
+const myBoxes = document.getElementsByClassName('block');
 
 
 // The Functionality
+function loopThrough() {
 
+  const changeColor = () => this.clasList.toggle('pink');
+
+  for (let i = 0; i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor, 1 * 1000);
+  };
+
+};
 
 // The Event Listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
