b019d417757117f9aff17a9a8e535b630a181990
Briana Bazail <brianabazail@ufl.edu>
2019-07-13 18:50:16 -0400

    Add css transitions

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 // Global Variables
 const myBoxes = document.getElementsByClassName('block');
 
-
 // The Functionality
 function loopThrough() {
 
-  const changeColor = () => this.clasList.toggle('pink');
+  const changeColor = () => this.classList.toggle('pink');
 
-  for (let i = 0; i < this.textContent.length * 2; i++) {
-    setTimeout(changeColor, 1 * 1000);
+  for(let i = 0; i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor, i * 1000);
   };
-
 };
 
 // The Event Listener
